e0a3792e135c64116912e691a77a0e59660b243d
Vítor Soares Vian <vitorsoaresvian@gmail.com>
2016-09-18 21:26:56 -0300

    Criando update

--- DBPDO.class.php
+++ DBPDO.class.php
@@ -96,8 +96,35 @@ class DBPDO
         {
             $prepare           = $this->mountPatternPrepare($data);
             $columns           = implode(array_keys((array)$data),',');
-            $values            = implode($prepare,',');
-            $queryPrepare      = "INSERT INTO {$table}($columns) VALUES({$values})";
+            $valuesPrepare     = implode($prepare,',');
+            $queryPrepare      = "INSERT INTO {$table}($columns) VALUES({$valuesPrepare})";
+            $connectionPrepare = $this->connection->prepare($queryPrepare);
+            $connectionPrepare = $this->replacePatternBind($data,$connectionPrepare,$table);
+            return $connectionPrepare->execute();
+        }
+    }
+
+    /**
+     * Faz um insert no banco de dados
+     *
+     * @param $tabela
+     * @param $dados
+     */
+    public function update($table,$data,$id)
+    {
+        if ($this->paramsIsNotNull(func_get_args()))
+        {
+            $prepare           = $this->mountPatternPrepare($data,true);
+            foreach ($prepare as $column => $prep)
+            {
+                $update[] = "{$column} = {$prep}";
+            }
+            $update = implode($update,',');
+            $where = ["table_schema = 'imigration'",
+                      "table_name = 'teste'",
+                      "column_key = 'PRI'"];
+            $columnPrimaryKey  = $this->getConfigurationDB("column_name","columns",$where,false);
+            $queryPrepare      = "UPDATE {$table} SET $update WHERE {$columnPrimaryKey['column_name']} = {$id}";
             $connectionPrepare = $this->connection->prepare($queryPrepare);
             $connectionPrepare = $this->replacePatternBind($data,$connectionPrepare,$table);
             return $connectionPrepare->execute();
@@ -109,15 +136,22 @@ class DBPDO
      *  para operaçoes esta se usando o padrao ':nome_da_coluna' ou ':nome_do_valor'
      *
      * @param $data -> Dados que deseja aplicar o padrao
+     * @param $assoc -> Passar true quando desejar um array associoativo
      * @return array|bool
      */
-    public function mountPatternPrepare($data)
+    public function mountPatternPrepare($data,$assoc = false)
     {
         if ($this->paramsIsNotNull(func_get_args()) AND is_object($data))
         {
             foreach ($data as $column => $dat)
             {
-                $prepare[] = ":{$column}";
+                if ($assoc)
+                {
+                    $prepare[$column] = ":{$column}";
+                }else
+                {
+                    $prepare[] = ":{$column}";
+                }
             }
 
             return $prepare;
@@ -190,4 +224,44 @@ class DBPDO
 
         return $return;
     }
+
+    /**
+     * Retorna a configuração do banco de dados
+     *
+     * @param $select -> Quais as colunas que devem ser projetadas
+     * @param null $table -> Tabela que deve ser considerada
+     * @param null $column -> Coluna que deve ser retornada (where)
+     * @param null $database -> Banco de dados que deve ser considerado (where)
+     *                              Caso nao seja informado usara o banco usado na conexão.
+     * @param null $all -> Apenas um array ou vários arrays com informacoes dentro
+     * @return array
+     */
+    public function getConfigurationDB($select,$from,$where = "1=1",$all = true)
+    {
+        if ($this->paramsIsNotNull(array($select,$from)))
+        {
+            if (is_array($select))
+            {
+                $select = implode(",",$select);
+            }
+
+            if (is_array($where))
+            {
+                $where = implode(" AND ",$where);
+            }
+
+            $query = "SELECT $select
+                              FROM information_schema.{$from} 
+                          WHERE {$where}";
+            $data = $this->connection->query($query);
+
+            if ($all) {
+                return $data->fetchAll(PDO::FETCH_ASSOC);
+            }
+            return $data->fetch(PDO::FETCH_ASSOC);
+        }
+
+        return false;
+    }
+
 }
